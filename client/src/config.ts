3cd1cff91a506668c52f7c2ce608b0919e183c9f
Daniel Tortosa Marangoni <danielmarangoni@yahoo.com.br>
2021-05-04 17:44:08 -0300

    feat: Add environment settings

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,16 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+// API Gateway backend API ID
+const apiId = 'le8djvzu64';
+// AWS Region where the API Gateway backend API is deployed
+const region = 'sa-east-1';
+// API Gateway backend API endpoint
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`;
 
+// Serverless offline backend local endpoint
+// export const apiEndpoint = 'http://localhost:4000/dev';
+
+// Auth0 configurations - update it for you own APP
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
-}
+    domain: 'dtm.us.auth0.com',
+    clientId: 'bZZocuZ2VFCDJ5WAWJqHf9DJOeVYcaAP',
+    callbackUrl: 'http://localhost:3000/callback',
+};
